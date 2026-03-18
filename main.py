89397106858b3978732f9ea2f3bb4b88cde6613a
THEJESWAR21 <thejeswar21jeeva@gmail.com>
2026-03-18 14:48:18 +0530

    Understand the Code

--- main.py
+++ main.py
@@ -1,66 +1,77 @@
+# Make it easier to refer to file paths without worrying about platform specific notations
 import os.path
-import drafts
 
+# Used when refreshing expired authentication Tokens
 from google.auth.transport.requests import Request
+
+# To Store login / Authentication Data
 from google.oauth2.credentials import Credentials
+
+# Flows typically opens the system browser and directs the user to Google's Authorization for authentication
 from google_auth_oauthlib.flow import InstalledAppFlow
+
+
+# Used to create a connection to a Google APi Service
 from googleapiclient.discovery import build
+
+# To handle API request failures
 from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = ["https://www.googleapis.com/auth/gmail.compose", "https://www.googleapis.com/auth/gmail.readonly"]
+# Choosing a Scope
+SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+# readonly - View your Emails and Messages
 
 def main():
-  """Shows basic usage of the Gmail API.
-  Lists the user's Gmail labels.
-  """
-  creds = None
-  # The file token.json stores the user's access and refresh tokens, and is
-  # created automatically when the authorization flow completes for the first
-  # time.
-  if os.path.exists("token.json"):
-    creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-  # If there are no (valid) credentials available, let the user log in.
-  if not creds or not creds.valid:
-    if creds and creds.expired and creds.refresh_token:
-      creds.refresh(Request())
-    else:
-      flow = InstalledAppFlow.from_client_secrets_file(
-          "credentials.json", SCOPES
-      )
-      creds = flow.run_local_server(port=0)
-    # Save the credentials for the next run
-    with open("token.json", "w") as token:
-      token.write(creds.to_json())
-      print("Token File Generated!!!!")
+    """ Creating the Token File for authoriziation to user the Gmail API"""
 
-  try:
-
-       # Call the Gmail API
-        service = build("gmail", "v1", credentials=creds)
-        results = (
-            service.users().messages().list(userId="me", labelIds=["INBOX"]).execute()
-        )
-        messages = results.get("messages", [])
-
-        if not messages:
-            print("No messages found.")
-            return
-
-        print("Messages:")
-        for message in messages:
-            print(f'Message ID: {message["id"]}')
-            msg = (
-                service.users().messages().get(userId="me", id=message["id"]).execute()
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    
+    # if there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
             )
-            print(f'  Subject: {msg["snippet"]}')
+            creds = flow.run_local_server(port=0)
 
-  except HttpError as error:
-    print(f"An error occurred: {error}")
+        # Save the credentails for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
 
-  return creds
+        
+    try:
+        """ Call the Gmail API """
 
+        # Creates a Authorzied Gmail API Client service object using the credentails
+        service = build("gmail", "v1", credentials=creds)
 
+        # The result object contains a "labels" list
+        results = service.users().labels().list(userId="me").execute()
+
+        # Extracts a list of labels from a dictionary named results
+        labels = results.get("labels", [])
+
+        if not labels:
+            print("No Labels found.")
+            return
+        print("Labels:")
+        for label in labels:
+            print(label)
+
+    # Checks for Errors
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+    
 if __name__ == "__main__":
-  creds = main()
-  drafts.create_draft(creds)
+    main()
+        
+
+
+
+
